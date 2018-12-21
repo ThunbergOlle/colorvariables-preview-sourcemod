0a865871b5b7df858a9c132e262ec0dc11b085e5
ThunbergOlle <olle.thunberg03@gmail.com>
2018-12-21 21:33:49 +0100

    Compiled, update readme, installer :balloon:
    A shit ton of stuff got added.

---
+++ downloads.md
@@ -0,0 +1,13 @@
+#### Downloads
+You can find all the downloadable builds here
+
+**MAKE A FOLDER ON YOUR DESKTOP AND CHOOSE IT AS YOUR FILE DESTINATION** or your desktop will be filled with dll files and shit.
+
+[Color-Preview-win32-x64.exe (v0.1)]()
+[Color-Preview-win32-x64.zip (v0.1)]()
+
+
+
+
+##### Run
+Open the directory that you have chosed for your files and locate the file called "Color-Preview.exe". Run it.
--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow, globalShortcut} = require('electron')
-
+const path = require('path');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 400})
+  mainWindow = new BrowserWindow({width: 600, height: 400, icon: path.join(__dirname, 'appico.ico')
+})
   mainWindow.setMenu(null);
   mainWindow.setResizable(false);
   // and load the index.html of the app.
