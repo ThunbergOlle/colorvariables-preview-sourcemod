555ca8539970eee3e18e5de2ec6dc125fc2a7a8f
ThunbergOlle <olle.thunberg03@gmail.com>
2018-12-21 19:11:43 +0100

    Bug fixes :bug:

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, globalShortcut} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
   mainWindow.setMenu(null);
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+	globalShortcut.register('f5', function() {
+		mainWindow.reload()
+	})
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
